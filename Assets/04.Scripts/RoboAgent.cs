65b4a8da799845e4457fd3e490d6e6863a8ff114
shinjeongmin <sin37minduke@naver.com>
2023-09-02 13:48:39 +0900

    Modify joint number to upper, fore, wrist from only upper joint.

--- Assets/04.Scripts/RoboAgent.cs
+++ Assets/04.Scripts/RoboAgent.cs
@@ -19,15 +19,19 @@ public class RoboAgent : Agent
     public Transform neck;
     public Transform rib;
     public Transform left_upperarm;
-    public Transform start_left_upperarm;
-
     public Transform left_forearm;
     public Transform left_wrist;
     public Transform right_upperarm;
-    public Transform start_right_upperarm;
-
     public Transform right_forearm;
     public Transform right_wrist;
+
+    [Header("Start Transform")]
+    public Transform start_left_upperarm;
+    public Transform start_left_forearm;
+    public Transform start_left_wrist;
+    public Transform start_right_upperarm;
+    public Transform start_right_forearm;
+    public Transform start_right_wrist;
 
     // 초기 위치 저장 dictionary
     private Dictionary<string, Transform> jointItems = new Dictionary<string, Transform>();
@@ -45,9 +49,17 @@ public class RoboAgent : Agent
 
         // save start joint transform
         start_left_upperarm = new GameObject().transform;
+        start_left_forearm = new GameObject().transform;
+        start_left_wrist = new GameObject().transform;
         start_left_upperarm.localRotation = left_upperarm.localRotation;
+        start_left_forearm.localRotation = left_forearm.localRotation;
+        start_left_wrist.localRotation = left_wrist.localRotation;
         start_right_upperarm = new GameObject().transform;
+        start_right_forearm = new GameObject().transform;
+        start_right_wrist = new GameObject().transform;
         start_right_upperarm.localRotation = right_upperarm.localRotation;
+        start_right_forearm.localRotation = right_forearm.localRotation;
+        start_right_wrist.localRotation = right_wrist.localRotation;
 
         jointItems.Add("head", head);
         jointItems.Add("neck", neck);
@@ -71,7 +83,11 @@ public class RoboAgent : Agent
             fixedTime = 0;
 
             left_upperarm.localRotation = start_left_upperarm.localRotation;
+            left_forearm.localRotation = start_left_forearm.localRotation;
+            left_wrist.localRotation = start_left_wrist.localRotation;
             right_upperarm.localRotation = start_right_upperarm.localRotation;
+            right_forearm.localRotation = start_right_forearm.localRotation;
+            right_wrist.localRotation = start_right_wrist.localRotation;
         }
     }
 
@@ -85,9 +101,21 @@ public class RoboAgent : Agent
         sensor.AddObservation(jointItems["left_upperarm"].transform.localRotation.x);
         sensor.AddObservation(jointItems["left_upperarm"].transform.localRotation.y);
         sensor.AddObservation(jointItems["left_upperarm"].transform.localRotation.z);
+        sensor.AddObservation(jointItems["left_forearm"].transform.localRotation.x);
+        sensor.AddObservation(jointItems["left_forearm"].transform.localRotation.y);
+        sensor.AddObservation(jointItems["left_forearm"].transform.localRotation.z);
+        sensor.AddObservation(jointItems["left_wrist"].transform.localRotation.x);
+        sensor.AddObservation(jointItems["left_wrist"].transform.localRotation.y);
+        sensor.AddObservation(jointItems["left_wrist"].transform.localRotation.z);
         sensor.AddObservation(jointItems["right_upperarm"].transform.localRotation.x);
         sensor.AddObservation(jointItems["right_upperarm"].transform.localRotation.y);
         sensor.AddObservation(jointItems["right_upperarm"].transform.localRotation.z);
+        sensor.AddObservation(jointItems["right_forearm"].transform.localRotation.x);
+        sensor.AddObservation(jointItems["right_forearm"].transform.localRotation.y);
+        sensor.AddObservation(jointItems["right_forearm"].transform.localRotation.z);
+        sensor.AddObservation(jointItems["right_wrist"].transform.localRotation.x);
+        sensor.AddObservation(jointItems["right_wrist"].transform.localRotation.y);
+        sensor.AddObservation(jointItems["right_wrist"].transform.localRotation.z);
     }
 
     [Header("Joint Angle")]
@@ -99,21 +127,54 @@ public class RoboAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        Vector3 leftUpperArmcontrolSignal = Vector3.zero;
-        Vector3 rightUpperArmcontrolSignal = Vector3.zero;
         // left upper arm
+        Vector3 leftUpperArmcontrolSignal = Vector3.zero;
         leftUpperArmcontrolSignal.x = actionBuffers.ContinuousActions[0];
         leftUpperArmcontrolSignal.y = actionBuffers.ContinuousActions[1];
         leftUpperArmcontrolSignal.z = actionBuffers.ContinuousActions[2];
+
+        // left fore arm
+        Vector3 leftForeArmcontrolSignal = Vector3.zero;
+        leftForeArmcontrolSignal.x = actionBuffers.ContinuousActions[3];
+        leftForeArmcontrolSignal.y = actionBuffers.ContinuousActions[4];
+        leftForeArmcontrolSignal.z = actionBuffers.ContinuousActions[5];
+
+        // left wrist
+        Vector3 leftWristcontrolSignal = Vector3.zero;
+        leftWristcontrolSignal.x = actionBuffers.ContinuousActions[6];
+        leftWristcontrolSignal.y = actionBuffers.ContinuousActions[7];
+        leftWristcontrolSignal.z = actionBuffers.ContinuousActions[8];
+
         // right upper arm
-        rightUpperArmcontrolSignal.x = actionBuffers.ContinuousActions[3];
-        rightUpperArmcontrolSignal.y = actionBuffers.ContinuousActions[4];
-        rightUpperArmcontrolSignal.z = actionBuffers.ContinuousActions[5];
+        Vector3 rightUpperArmcontrolSignal = Vector3.zero;
+        rightUpperArmcontrolSignal.x = actionBuffers.ContinuousActions[9];
+        rightUpperArmcontrolSignal.y = actionBuffers.ContinuousActions[10];
+        rightUpperArmcontrolSignal.z = actionBuffers.ContinuousActions[11];
+
+        // right fore arm
+        Vector3 rightForeArmcontrolSignal = Vector3.zero;
+        rightForeArmcontrolSignal.x = actionBuffers.ContinuousActions[12];
+        rightForeArmcontrolSignal.y = actionBuffers.ContinuousActions[13];
+        rightForeArmcontrolSignal.z = actionBuffers.ContinuousActions[14];
+
+        // right wrist
+        Vector3 rightWristcontrolSignal = Vector3.zero;
+        rightWristcontrolSignal.x = actionBuffers.ContinuousActions[15];
+        rightWristcontrolSignal.y = actionBuffers.ContinuousActions[16];
+        rightWristcontrolSignal.z = actionBuffers.ContinuousActions[17];
 
         jointItems["left_upperarm"].transform.localRotation
             = Quaternion.Euler(jointItems["left_upperarm"].transform.localRotation.eulerAngles + leftUpperArmcontrolSignal * angle);
+        jointItems["left_forearm"].transform.localRotation
+            = Quaternion.Euler(jointItems["left_forearm"].transform.localRotation.eulerAngles + leftForeArmcontrolSignal * angle);
+        jointItems["left_wrist"].transform.localRotation
+            = Quaternion.Euler(jointItems["left_wrist"].transform.localRotation.eulerAngles + leftWristcontrolSignal * angle);
         jointItems["right_upperarm"].transform.localRotation
             = Quaternion.Euler(jointItems["right_upperarm"].transform.localRotation.eulerAngles + rightUpperArmcontrolSignal * angle);
+        jointItems["right_forearm"].transform.localRotation
+            = Quaternion.Euler(jointItems["right_forearm"].transform.localRotation.eulerAngles + rightForeArmcontrolSignal * angle);
+        jointItems["right_wrist"].transform.localRotation
+            = Quaternion.Euler(jointItems["right_wrist"].transform.localRotation.eulerAngles + rightWristcontrolSignal * angle);
 
         // 판이 떨어진 경우
         if (targetCube.transform.localPosition.y < .5f)
